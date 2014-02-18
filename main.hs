f5b8a2ea8fbe23ca969589b9ff9c193b9bb5b0df
Alex Neslusan <alex.neslusan@harman.com>
2014-02-18 18:32:04 +0800

    Changed output

--- main.hs
+++ main.hs
@@ -2,17 +2,33 @@
 import Control.Monad
 import Text.JSON
 import System.Environment
+import qualified Data.Text as T
 
-import qualified ParseJSON as P
+import ParseJSON
+
+strip :: String -> String
+strip = T.unpack . T.strip . T.pack
+
+values :: Module -> [String]
+values (Module name values) =
+	map (\(Value n t) -> name ++ "." ++ n ++ " : " ++ show' t) values
+
+show' :: Type -> String
+show' (Function args result) = (concatMap (\arg -> strip (show' arg) ++ " -> ") args) ++ strip (show' result)
+show' (Var name) = name
+show' (Data name args) = case name of
+	"_List" -> "[" ++ (concatMap show' args) ++ "]"
+	_ -> name ++ " " ++ (concatMap show' args)
+show' (Record fields extension) = "{" ++ (concatMap (\(Field n t) -> n ++ ": " ++ show' t) fields) ++ "}"
 
 main :: IO ()
 main = do
     (f:_) <- getArgs
     file <- readFile f
-    stuff <- case (decode file :: Result P.Docs) of
+    stuff <- case (decode file :: Result Docs) of
         Ok idk -> return idk
         _ -> undefined
     ms <- case stuff of
-        P.Docs x -> return x
-        P.Doc  x -> return [x]
-    mapM_ (\m -> print m) ms
+        Docs x -> return x
+        Doc  x -> return [x]
+    mapM_ (\m -> print $ values m) ms
